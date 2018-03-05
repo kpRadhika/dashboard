3186bf47919b547e05d0cccf1d0ca71e9ef5d74e
kpRadhika <36230064+kpRadhika@users.noreply.github.com>
2018-03-05 16:39:11 +0530

    committing latest changes - By Radhika

--- src/com/parinati/leaves/LeaveApplicationDomain.java
+++ src/com/parinati/leaves/LeaveApplicationDomain.java
@@ -15,7 +15,7 @@ public class LeaveApplicationDomain {
 	public LeaveApplicationDomain(){
 		dbhelper = new DBConnectionManager();
 	}
-	
+
 	public List<ArrayList> fetchLeaveTypes(){
 		List<ArrayList> rs = null;
 		StringBuilder sql = new StringBuilder();
@@ -23,11 +23,11 @@ public class LeaveApplicationDomain {
 			sql.append(" SELECT LEAVETYPEID,    ");
 			sql.append(" LEAVETYPE              ");
 			sql.append(" FROM LEAVETYPEMASTER   ");
-			
+
 			rs = dbhelper.executeQuery(sql.toString(), null, null);
 		}
 		catch (Exception e) {
-			
+
 		}
 		return rs;
 	}
@@ -40,25 +40,23 @@ public class LeaveApplicationDomain {
 			sql = new StringBuilder();
 			sql.append(" SELECT                              ");
 			sql.append(" A.EMPID,                            ");
-			sql.append(" B.PROJECTLEADID,                    ");
+			sql.append(" C.REPORTINGMANAGERID,               ");
 			sql.append(" A.FNAME||' '||A.LNAME               ");
 			sql.append(" FROM                                ");
 			sql.append(" EMPLOYEEDTLS A,                     ");
-			sql.append(" EMPROLEDTLS C,                      ");
-			sql.append(" PROJECTMASTER B                     ");
+			sql.append(" EMPROLEDTLS C                       ");
 			sql.append(" WHERE                               ");
 			sql.append(" A.EMPID = C.EMPID                   ");
-			sql.append(" AND   C.PROJECTID = B.PROJECTID     ");
 			sql.append(" AND   A.OFFICIALEMAIL = ?           ");
-			
-			values = new ArrayList<>();			
+
+			values = new ArrayList<>();
 			values.add(valueList.get(0));
-			
+
 			valueTypes = new ArrayList<>();
 			valueTypes.add(GenericConstDef.DB_STRING);
-			
+
 			List rs = dbhelper.executeQuery(sql.toString(), values, valueTypes);
-			
+
 			if(rs!=null && !rs.isEmpty()){
 				sql = new StringBuilder();
 				sql.append(" INSERT                     ");
@@ -82,8 +80,8 @@ public class LeaveApplicationDomain {
 				sql.append("?,                          ");
 				sql.append("?                           ");
 				sql.append(")                           ");
-				
-				values = new ArrayList<>();			
+
+				values = new ArrayList<>();
 				values.add(Integer.parseInt((String)((ArrayList)rs.get(0)).get(0)));
 				values.add(valueList.get(3));
 				values.add(valueList.get(1));
@@ -91,7 +89,7 @@ public class LeaveApplicationDomain {
 				values.add("P");
 				values.add(valueList.get(0));
 				values.add(Integer.parseInt((String)((ArrayList)rs.get(0)).get(1)));
-				
+
 				valueTypes = new ArrayList<>();
 				valueTypes.add(GenericConstDef.DB_INT);
 				valueTypes.add(GenericConstDef.DB_INT);
@@ -100,31 +98,31 @@ public class LeaveApplicationDomain {
 				valueTypes.add(GenericConstDef.DB_STRING);
 				valueTypes.add(GenericConstDef.DB_STRING);
 				valueTypes.add(GenericConstDef.DB_INT);
-				
+
 				int result = dbhelper.executeInsertUpdate(sql.toString(), values, valueTypes);
 				if(result>0){
 					sql = new StringBuilder();
 					sql.append(" SELECT FNAME, OFFICIALEMAIL FROM EMPLOYEEDTLS WHERE EMPID = ? ");
-					
+
 					values = new ArrayList<>();
 					values.add(Integer.parseInt((String)((ArrayList)rs.get(0)).get(1)));
-					
+
 					valueTypes = new ArrayList<>();
 					valueTypes.add(GenericConstDef.DB_INT);
-					
+
 					List managerDetails = dbhelper.executeQuery(sql.toString(), values, valueTypes);
-					
+
 					if(managerDetails!=null && !managerDetails.isEmpty()){
-					
+
 					String mailTempPath = FileReadUtil.getValue("LEAVEAPPMAIL");
 					String[] var = new String[2];
 					var[0] = (String)((ArrayList)managerDetails.get(0)).get(0);
 					var[1] = valueList.get(0).toString();
-					
-					new MailerUtil().postMailWithTLSAuth((String)((ArrayList)managerDetails.get(0)).get(1), "Leave Application Pending for action", mailTempPath, var);
+
+					new MailerUtil().postMailWithTLSAuth((String)((ArrayList)managerDetails.get(0)).get(1),(String)valueList.get(0), "Leave Application Pending for action", mailTempPath, var);
 					}
 				}
-				
+
 			}
 		}
 		catch (Exception e) {
@@ -208,6 +206,22 @@ public class LeaveApplicationDomain {
 			queryTypes.add(GenericConstDef.DB_INT);
 
 			rs = dbhelper.executeInsertUpdate(sql.toString(), queryValues, queryTypes);
+			if(rs > 0){
+				sql = new StringBuilder();
+				sql.append(" SELECT FNAME, OFFICIALEMAIL FROM EMPLOYEEDTLS WHERE EMPID = ? ");
+				queryValues = new ArrayList<>();
+				queryValues.add(Integer.parseInt((String)inputParam.get(3)));
+				queryTypes = new ArrayList<>();
+				queryTypes.add(GenericConstDef.DB_INT);
+				List managerDetails = dbhelper.executeQuery(sql.toString(), queryValues, queryTypes);
+				if(managerDetails!=null && !managerDetails.isEmpty()){
+				String mailTempPath = FileReadUtil.getValue("LEAVEAPPROVEMAIL");
+				String[] var = new String[2];
+				var[0] = (String)((ArrayList)managerDetails.get(0)).get(0);
+				var[1] = (String)((ArrayList)managerDetails.get(0)).get(0);
+				new MailerUtil().postMailWithTLSAuth((String)((ArrayList)managerDetails.get(0)).get(1),(String)inputParam.get(2), "Leave Application action", mailTempPath, var);
+				}
+			}
 		}
 		catch (Exception e) {
 			CustomLogger.exceptionJava(e, "Exception in updateEmpLeaveDetail() while executing the query:"+sql.toString(), "LeaveApplicationDomain.java");
--- src/com/parinati/util/MailerUtil.java
+++ src/com/parinati/util/MailerUtil.java
@@ -15,18 +15,18 @@ import javax.mail.internet.MimeMessage;
 
 
 public class MailerUtil {
-	
+
 	public MailerUtil() {
-		
+
 	}
-	
+
 	private static final String MAIL_SERVER_IP;
 	private static final String MAIL_SERVER_PORT;
 	private static final String MAIL_FROM;
 	private static final String PASSWORD;
 	private static final String NO_REPLY_ID;
 	private static final String MAIL_CC;
-	
+
 	static {
 		Properties props = new Properties();
 
@@ -48,12 +48,12 @@ public class MailerUtil {
 	/**********************************************************************************
 	 * Method Description : This method is used to send post mail with cc
 	 * options and TLS Authentication.
-	 * 
+	 *
 	 ********************************************************************************/
 
-	public void postMailWithTLSAuth(String toEmail, String subject, String mailTempPath,String[] varData ) {
-		try {	
-			
+	public void postMailWithTLSAuth(String toEmail,String ccMail, String subject, String mailTempPath,String[] varData ) {
+		try {
+
 			String body = setMailTemplateData(mailTempPath,varData);
 			Properties props = new Properties();
 			props.put("mail.smtp.ssl.trust", MAIL_SERVER_IP);
@@ -61,7 +61,7 @@ public class MailerUtil {
 			props.put("mail.smtp.port", MAIL_SERVER_PORT); //TLS Port
 			props.put("mail.smtp.auth", "true"); //enable authentication
 			props.put("mail.smtp.starttls.enable", "true"); //enable STARTTLS
-			
+
 	                //create Authenticator object to pass in Session.getInstance argument
 			Authenticator auth = new Authenticator() {
 				//override the getPasswordAuthentication method
@@ -70,18 +70,18 @@ public class MailerUtil {
 				}
 			};
 			Session session = Session.getInstance(props, auth);
-			
-			sendEmail(session, toEmail, subject, body);
-			
+
+			sendEmail(session, toEmail, ccMail, subject, body);
+
 		} catch (Exception exception) {
 			CustomLogger.exceptionJava(exception, "MailerUtil.java:: postMail()", "MailerUtil.java");
 		}
 	}
 
-	
+
 	/**********************************************************************************
 	 * Method Description : This method is used to form mail content from a template
-	 * 
+	 *
 	 ***********************************************************************************/
 
 	public String setMailTemplateData(String filePath, String[] varData) {
@@ -100,11 +100,11 @@ public class MailerUtil {
 
 	/**********************************************************************************
 	 * Method Description : This method is used to send mail with CC Field
-	 
+
 	 ***********************************************************************************/
 
 
-	public static void sendEmail(Session session, String toEmail, String subject, String body){
+	public static void sendEmail(Session session, String toEmail, String ccMail, String subject, String body){
 		try
 	    {
 	      MimeMessage msg = new MimeMessage(session);
@@ -124,9 +124,9 @@ public class MailerUtil {
 	      msg.setSentDate(new Date());
 
 	      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-	      msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(MAIL_CC, false));
-	      
-    	  Transport.send(msg);  
+	      msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccMail, false));
+
+    	  Transport.send(msg);
 
 	    }
 	    catch (Exception e) {
